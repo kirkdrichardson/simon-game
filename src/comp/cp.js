fd0a766c73f5d152da88885ab43e2646eadd6ccb
ghost <kirk.douglas.richardson@gmail.com>
2017-09-09 00:26:21 -0600

    buttons connected

--- src/comp/cp.js
+++ src/comp/cp.js
@@ -6,11 +6,33 @@ class ControlPanel extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      count: '--'
+      count: '--',
+      started: false,
+      strictMode: false
     }
   }
+  toggleStrict = () => {
+    this.setState({
+      strictMode: !this.state.strictMode
+    });
+  }
+
+  toggleStart = () => {
+    this.setState({
+      count: 1,
+      started: !this.state.started
+    });
+  }
+
+  toggleColor = (event) => {
+    console.log(event.target.id)
+  }
+
   render() {
-    const strictLight = {backgroundColor: '#D13045'}
+    const strictLight = {backgroundColor: '#0C001E'};
+    if (this.state.strictMode)
+      strictLight.backgroundColor = '#D13045';
+
     return (
       <div className='game-container'>
 
@@ -21,24 +43,38 @@ class ControlPanel extends Component {
           <div id='count'>{this.state.count}</div>
           <div id='btns-container'>
             <div id='start-btn-container'>
-              <div id='start-btn'></div>
+              <div id='start-btn' onClick={this.toggleStart}></div>
               <p className='btn-text'>Start</p>
             </div>
               <div id='strict-btn-container'>
-                <div style={strictLight} id='strict-light'></div>
-                <div id='strict-btn'></div>
+                <div id='strict-light' style={strictLight}></div>
+                <div id='strict-btn' onClick={this.toggleStrict}></div>
                 <p className='btn-text'>Strict</p>
               </div>
           </div>
         </div>
 
 
+        <div
+          id='red'
+          className='colored-div'
+          onClick={this.toggleColor}></div>
 
+        <div
+          id='green'
+          className='colored-div'
+          onClick={this.toggleColor}></div>
 
-        <div id='red' className='colored-div'></div>
-        <div id='green' className='colored-div'></div>
-        <div id='blue' className='colored-div'></div>
-        <div id='yellow' className='colored-div'></div>
+        <div
+          id='blue'
+          className='colored-div'
+          onClick={this.toggleColor}></div>
+
+        <div
+          id='yellow'
+          className='colored-div'
+          onClick={this.toggleColor}></div>
+
       </div>
     );
   }
