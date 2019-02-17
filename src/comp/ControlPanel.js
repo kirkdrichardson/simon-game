927d325a44f9db00659b6ad2596df55d13765e0d
Kirk <kirk.douglas.richardson@gmail.com>
2019-02-16 18:49:56 -0700

    began flow typing and reorganizing data structures

--- src/comp/ControlPanel.js
+++ src/comp/ControlPanel.js
@@ -1,51 +1,60 @@
 //@flow strict
-import * as React from 'react';
-import styled, { css } from 'styled-components';
+import * as React from "react";
+import styled, { css } from "styled-components";
 
 // sounds associated with each of the colors. Used in handleUserMove
-const soundMap = {
+const SoundMap: { [string]: string } = {
   red: "https://s3.amazonaws.com/freecodecamp/simonSound1.mp3",
   green: "https://s3.amazonaws.com/freecodecamp/simonSound2.mp3",
   blue: "https://s3.amazonaws.com/freecodecamp/simonSound4.mp3",
   yellow: "https://s3.amazonaws.com/freecodecamp/simonSound3.mp3",
-  correct: "http://freesound.org/people/suntemple/sounds/253177/download/253177__suntemple__retro-accomplished-sfx.wav",
-  incorrect: "https://freesound.org/people/myfox14/sounds/382310/download/382310__myfox14__game-over-arcade.wav"
+  correct:
+    "http://freesound.org/people/suntemple/sounds/253177/download/253177__suntemple__retro-accomplished-sfx.wav",
+  incorrect:
+    "https://freesound.org/people/myfox14/sounds/382310/download/382310__myfox14__game-over-arcade.wav"
 };
 
+const ColorMap = {
+  red: "#FF93A2",
+  green: "#7EFF2D",
+  yellow: "#FFFCE0",
+  blue: "#7CD5FF"
+};
+
+type ColorType = $Keys<ColorMap>;
+
+const ColorValues: ColorType[] = Object.keys(ColorMap);
+
 type State = {
-  count: string,
+  count: number,
   started: boolean,
+  gameOver: boolean,
+  win: boolean,
   strictMode: boolean,
   userTurn: boolean,
-  sequenceArray: string[],
+  sequenceArr: string[],
   userPlay: number,
   replay: boolean,
-  gameOver: boolean,
   prompt: string
 };
 
 export default class ControlPanel extends React.Component<{}, State> {
-
   state = {
-      count: '--',
-      started: false,
-      strictMode: false,
-      userTurn: false,
-      sequenceArr: [],
-      userPlay: 0,
-      replay: false,
-      gameOver: false,
-      prompt: 'Press start to begin'
-    };
+    count: 0,
+    started: false,
+    gameOver: false,
+    win: false,
+    strictMode: false,
+    userTurn: false,
+    sequenceArr: [],
+    userPlay: 0,
+    replay: false,
+    prompt: "Press start to begin"
+  };
 
   componentDidUpdate() {
-    const {
-      count,
-      userPlay,
-      gameOver,
-      started
-    } = this.state;
-    
+    const { count, userPlay, gameOver, started, userTurn } = this.state;
+
     // user wins game if 20 steps completed
     if (count === 21) {
       this.onGameWin();
@@ -58,7 +67,7 @@ export default class ControlPanel extends React.Component<{}, State> {
 
     // modify count & play success sound if the user has played all colors in sequence & game not over
     if (count === userPlay && !gameOver) {
-      const successSound = new Audio(soundmap.correct);
+      const successSound = new Audio(SoundMap.correct);
       successSound.play();
 
       this.setState({
@@ -68,142 +77,146 @@ export default class ControlPanel extends React.Component<{}, State> {
     }
   }
 
-
   /********************   GAME CONTROL FUNCTION   ********************/
   toggleStrict = () => {
-    this.setState({
-      strictMode: !this.state.strictMode,
-    });
-  }
+    this.setState(prevState => ({ strictMode: !prevState.strictMode }));
+  };
 
   toggleStart = () => {
-    this.resetBoard();
-    const count = !this.state.started ? 1 : '--'
-    const prompt = !this.state.started ? 'Get 20 in a row to win!' : 'Press start to begin'
-    this.setState({
-      count: count,
-      started: !this.state.started,
-      userTurn: false,
-      sequenceArr: [],
-      prompt: prompt
+    this.resetBoard(() => {
+      const prompt = !this.state.started
+        ? "Get 20 in a row to win!"
+        : "Press start to begin";
+      this.setState(
+        {
+          count: 1,
+          userTurn: false,
+          sequenceArr: [],
+          prompt: prompt
+        },
+        () => {
+          setTimeout(() => this.setState({ prompt: "" }), 3000);
+        }
+      );
     });
-    setTimeout(() => this.setState({prompt: ''}), 3000);
-  }
+  };
 
   /********************   GAME EVENT LOGIC   ********************/
 
-  resetBoard = () => {
-    this.setState({
-      count: '--',
-      started: false,
-      userTurn: false,
-      sequenceArr: [],
-      userPlay: 0,
-      replay: false,
-      gameOver: false,
-      prompt: 'Press start to begin'
-    });
-  }
+  resetBoard = (cb: () => void) => {
+    this.setState(
+      {
+        count: 0,
+        started: false,
+        userTurn: false,
+        sequenceArr: [],
+        userPlay: 0,
+        replay: false,
+        gameOver: false,
+        prompt: "Press start to begin"
+      },
+      cb
+    );
+  };
 
   onGameWin = () => {
-    this.setState({
-      gameOver: true,
-      count: 'win!',
-      prompt: 'You broke the machine!'
-    });
+    this.setState(
+      {
+        gameOver: true,
+        win: true,
+        prompt: "You broke the machine!"
+      },
+      () => {
+        // loop success sound
+        const gameWinSound: HTMLAudioElement = new Audio(SoundMap.correct);
+        gameWinSound.loop = true;
+        gameWinSound.play();
+        setTimeout(() => (gameWinSound.loop = false), 5000);
 
-    // loop success sound
-    const gameWinSound = new Audio(soundmap.correct);
-    gameWinSound.loop = true
-    gameWinSound.play();
-    setTimeout(() => gameWinSound.loop = false, 5000);
-
-    // loop flashing lights
-    let cnt = 0
-    const id = setInterval(() => {
-      if (cnt === 5) {
-        clearInterval(id);
-        this.resetBoard();
+        // loop flashing lights
+        let cnt = 0;
+        const id = setInterval(() => {
+          if (cnt === 5) {
+            clearInterval(id);
+            this.resetBoard();
+          } else this.flashColorsOnReset();
+          cnt++;
+        }, 900);
       }
-      else
-        this.flashColorsOnReset();
-      cnt++;
-    }, 900);
-  }
+    );
+  };
 
-  flashColorsOnReset = () => {
+  flashColorsOnReset = (): void => {
     const colors = ["red", "green", "yellow", "blue"];
     for (let i = 0; i < colors.length; i++) {
       let color = colors[i];
       this.simulateClick(document.getElementById(color), color);
     }
-  }
-
+  };
 
   // toggle class to simulate a user click
-  simulateClick = (elem, color) => {
-    const colorMap = {red: '#FF93A2', green: '#7EFF2D', yellow: '#FFFCE0', blue: '#7CD5FF' }
-    elem.style.backgroundColor = colorMap[color];
-    setTimeout(() => {elem.style.backgroundColor = ''}, 600);
-  }
-
+  simulateClick = (elem: HTMLDivElement, color: ColorType) => {
+    elem.style.backgroundColor = ColorMap[color];
+    setTimeout(() => {
+      elem.style.backgroundColor = "";
+    }, 600);
+  };
 
   /********************   GAME SIMULATION LOGIC   ********************/
 
-computerTurn = () => {
-  // only run if a single new color hasn't yet been added to the sequence
-  // count will be updated after user moves correctly
-  if (this.state.count > this.state.sequenceArr.length || this.state.replay) {
-    const randomColor = () => ["red", "green", "blue", "yellow"][Math.floor(Math.random() * 4)];
-    // if replay, reuse sequence, else add new color to end
-    let sequence = (this.state.replay) ? this.state.sequenceArr : this.state.sequenceArr.concat(randomColor());
+  computerTurn = () => {
+    const { count, sequenceArr, replay } = this.state;
 
-    // for every color in sequnce, play sound & simulate click
-    const iterateOverSequence = (sequence) => {
+    // only run if a single new color hasn't yet been added to the sequence
+    // count will be updated after user moves correctly
+    if (count > sequenceArr.length || replay) {
+      const randomColor = () => ColorValues[Math.floor(Math.random() * 4)];
+      // if replay, reuse sequence, else add new color to end
+      let sequence = replay ? sequenceArr : sequenceArr.slice().concat(randomColor());
+
+      // for every color in sequnce, play sound & simulate click
+      const iterateOverSequence = sequence => {
         sequence.forEach(function(color, i) {
-          setTimeout(function() {
-            let element = document.getElementById(color);
-            let audioObj = new Audio(soundmap[color]);
-            this.simulateClick(element, color);
-            audioObj.play();
-          }.bind(this), i * 700);
+          setTimeout(
+            function() {
+              let element: HTMLElement | null = document.getElementById(color);
+              let audioObj: HTMLAudioElement = new Audio(SoundMap[color]);
+              this.simulateClick(element, color);
+              audioObj.play();
+            }.bind(this),
+            i * 700
+          );
         }, this);
+      };
+
+      const endingCallback = () => {
+        iterateOverSequence(this.state.sequenceArr);
+        this.setState({
+          userPlay: 0,
+          replay: false,
+          userTurn: true
+        });
+      };
+
+      this.setState(
+        {
+          sequenceArr: sequence
+        },
+        endingCallback
+      );
     }
+  };
 
-    const endingCallback = () => {
-      iterateOverSequence(this.state.sequenceArr)
-      this.setState({
-        userPlay: 0,
-        replay: false,
-        userTurn: true
-      });
-    }
-
-    this.setState({
-      sequenceArr: sequence
-    }, endingCallback);
-  }
-}
-
-
-  handleUserMove = (event) => {
-
-    const {
-      started,
-      userTurn,
-      sequenceArr,
-      strictMode
-    } = this.state;
+  handleUserMove = event => {
+    const { started, userTurn, sequenceArr, strictMode } = this.state;
 
     if (started && userTurn) {
-
       // TODO - change to a non-DOM paradigm
       // play sound on button press
       const color = event.target.id;
-
-      const audioObj = new Audio(soundmap[color]);
+      // $FlowFixMe
+      const audioObj: HTMLAudioElement = new Audio(SoundMap[color]);
       audioObj.play();
-
 
       // if user toggles incorrectly
       if (color !== sequenceArr[this.state.userPlay]) {
@@ -213,14 +226,14 @@ computerTurn = () => {
             clearInterval(id);
 
             // reset state & start at one
-            if (this.state.strictMode) {
+            if (strictMode) {
               this.resetBoard();
               this.setState({
                 count: 0,
                 started: true,
-                prompt: "To prevent restarting, turn strict mode off",
+                prompt: "To prevent restarting, turn strict mode off"
               });
-              setTimeout(() => this.setState({ prompt: '' }), 5000);
+              setTimeout(() => this.setState({ prompt: "" }), 5000);
             }
 
             // if strictMode off, turn on replay, set user play to 0, and let computer move
@@ -229,61 +242,66 @@ computerTurn = () => {
                 replay: true,
                 userPlay: 0,
                 userTurn: false
-              })
+              });
             }
-          }
-          else {
+          } else {
             // flash colors & play failure sound in every case
             this.flashColorsOnReset();
-            const failureSound = new Audio(soundmap.incorrect);
+            const failureSound = new Audio(SoundMap.incorrect);
             failureSound.play();
           }
 
-        cnt++;
-
+          cnt++;
         }, 900);
-
       }
       // if user plays correct color, play sound & add 1 to userPlay (used to evaluate the next index of sequenceArr)
       else {
-        this.setState((prevState) => ({ userPlay: prevState.userPlay + 1 }));
+        this.setState(prevState => ({ userPlay: prevState.userPlay + 1 }));
       }
     }
+  };
+
+
+  /********************   UI LOGIC   ********************/
+
+  // return the appropriate value to display to the user based on game state
+  getDisplay = (): string => {
+    const { started, gameOver, win, count } = this.state;
+    if (!started) return '--';
+    if (gameOver) return win ? 'win' : 'xx';
+    return String(count);
   }
 
-
   render() {
-
-    const { count, strictMode, prompt } = this.state;
+    const { count, strictMode, prompt, started } = this.state;
 
     return (
       <MainContainer>
-      <GameWrapper>
-        <ScoreAndButtons>
-          <Title>Simon</Title>
-          <Count>{count}</Count>
-          <ControlButtons>
-            <ControlButtonContainer margin='0 45px 0 0'>
-              <ControlButton onClick={this.toggleStart} />
-              <ButtonText>Start</ButtonText>
-            </ControlButtonContainer>
+        <GameWrapper>
+          <ScoreAndButtons>
+            <Title>Simon</Title>
+            <Count>{this.getDisplay()}</Count>
+            <ControlButtons>
+              <ControlButtonContainer margin="0 45px 0 0">
+                <ControlButton onClick={this.toggleStart} />
+                <ButtonText>Start</ButtonText>
+              </ControlButtonContainer>
               <ControlButtonContainer>
                 <ButtonIndicatorLight on={strictMode} />
                 <ControlButton warning onClick={this.toggleStrict} />
                 <ButtonText>Strict</ButtonText>
               </ControlButtonContainer>
-          </ControlButtons>
-        </ScoreAndButtons>
-        {['red', 'green', 'blue', 'yellow'].map(t =>
-          <ColoredButton type={t} onClick={this.handleUserMove} />
-        )}
-      </GameWrapper>
-      <Prompt>{prompt}</Prompt>
+            </ControlButtons>
+          </ScoreAndButtons>
+          {["red", "green", "blue", "yellow"].map(t => (
+            <ColoredButton type={t} id={t} onClick={this.handleUserMove} />
+          ))}
+        </GameWrapper>
+        <Prompt>{prompt}</Prompt>
       </MainContainer>
     );
   }
 }
-
 
 // style
 
@@ -297,21 +315,21 @@ const MainContainer = styled.div`
 `;
 
 const GameWrapper = styled.div`
-  border: solid 15px #0C001E;
+  border: solid 15px #0c001e;
   height: 600px;
   width: 600px;
   display: flex;
   flex-wrap: wrap;
   justify-content: space-around;
   align-items: center;
-  background-color: #0C001E;
+  background-color: #0c001e;
   border-radius: 40%;
   position: relative;
   box-shadow: 2px 2px 5px #666;
 `;
 
 const Title = styled.h1`
-  font-family: 'Gravitas One', 'Orbitron', cursive, sans-serif;
+  font-family: "Gravitas One", "Orbitron", cursive, sans-serif;
   font-size: 50px;
   margin-bottom: 10px;
   margin-top: 20px;
@@ -321,14 +339,14 @@ const Count = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  background-color: #0C001E;
+  background-color: #0c001e;
   height: 90px;
   width: 150px;
   border-radius: 15%;
   font-size: 78px;
   font-weight: bold;
-  color: #D13045;
-  font-family: 'Digital','Orbitron', sans-serif;
+  color: #d13045;
+  font-family: "Digital", "Orbitron", sans-serif;
   border: solid 5px gray;
   padding-bottom: 10px;
   box-shadow: 1px 1px 5px #888888;
@@ -337,11 +355,11 @@ const Count = styled.div`
 const ScoreAndButtons = styled.div`
   position: absolute;
   z-index: 2;
-  background-color: #E0D2BC;
+  background-color: #e0d2bc;
   height: 320px;
   width: 320px;
   border-radius: 40%;
-  border: solid 30px #0C001E;
+  border: solid 30px #0c001e;
   display: flex;
   justify-content: center;
   align-items: center;
@@ -357,21 +375,21 @@ const ControlButtons = styled.div`
 const ControlButtonContainer = styled.div`
   display: flex;
   flex-direction: column;
-  margin: ${props => props.margin ? props.margin : '0 0 0 45px'};
+  margin: ${props => (props.margin ? props.margin : "0 0 0 45px")};
   line-height: 1px;
 `;
 
 const ControlButton = styled.div`
   height: 30px;
   width: 30px;
-  margin-top: ${props => props.warning ? 0 : 8}px;
+  margin-top: ${props => (props.warning ? 0 : 8)}px;
   border-radius: 50%;
   border: solid 5px black;
-  background-color:  ${props => props.warning ? '#EAD746' : '#D13045'};
+  background-color: ${props => (props.warning ? "#EAD746" : "#D13045")};
   box-shadow: 2px 2px 3px #888888;
 
   :hover {
-    background-color: ${props => props.warning ? '#DDCB42' : '#BA1A3C'};
+    background-color: ${props => (props.warning ? "#DDCB42" : "#BA1A3C")};
     box-shadow: 0 2px #666;
     transform: translateY(1px);
   }
@@ -385,9 +403,9 @@ const ButtonIndicatorLight = styled.div`
   height: 10px;
   width: 10px;
   border-radius: 50%;
-  background-color: ${props => props.on ? '#D13045' : '#0C001E'};
+  background-color: ${props => (props.on ? "#D13045" : "#0C001E")};
   margin: 0 0 0 40px;
-  border: solid .9px black;
+  border: solid 0.9px black;
   box-shadow: 1px 1px 1px #888888;
 `;
 
@@ -400,50 +418,49 @@ const ColoredButton = styled.div`
   z-index: 1;
 
   ${props => {
-    switch(props.type) {
-      case 'red':
+    switch (props.type) {
+      case "red":
         return css`
           border-top-left-radius: 80%;
-          background-color: #D13045;
+          background-color: #d13045;
           :active {
-            background-color: #BA1A3C;
+            background-color: #ba1a3c;
             box-shadow: 0 2px #666;
             transform: translateY(1px);
           }
         `;
-        case 'green':
-          return css`
-            border-top-right-radius: 80%;
-            background-color: #51BC0F;
-            :active {
-              background-color: #4CAF0E;
-              box-shadow: 0 2px #666;
-              transform: translateY(1px);
-            }
-          `;
-        case 'blue':
-            return css`
-              border-bottom-left-radius: 80%;
-              background-color: #31AFEA;
-              :active {
-                background-color: #2DA2D8;
-                box-shadow: 0 2px #666;
-                transform: translateY(1px); 
-              }
-            `;
-        case 'yellow':
-            return css`
-              border-bottom-right-radius: 80%;
-              background-color: #EAD746;
-              :active {
-                background-color: #DDCB42;
-                box-shadow: 0 2px #666;
-                transform: translateY(1px);
-              }
-            `;
-        }
+      case "green":
+        return css`
+          border-top-right-radius: 80%;
+          background-color: #51bc0f;
+          :active {
+            background-color: #4caf0e;
+            box-shadow: 0 2px #666;
+            transform: translateY(1px);
+          }
+        `;
+      case "blue":
+        return css`
+          border-bottom-left-radius: 80%;
+          background-color: #31afea;
+          :active {
+            background-color: #2da2d8;
+            box-shadow: 0 2px #666;
+            transform: translateY(1px);
+          }
+        `;
+      case "yellow":
+        return css`
+          border-bottom-right-radius: 80%;
+          background-color: #ead746;
+          :active {
+            background-color: #ddcb42;
+            box-shadow: 0 2px #666;
+            transform: translateY(1px);
+          }
+        `;
     }
-  }
+  }}
 `;
 
 const Prompt = styled.p`
@@ -454,5 +471,5 @@ const Prompt = styled.p`
   font-weight: bold;
   margin: 20px 0;
   padding: 0;
-  font-family: 'Ubuntu', sans-serif;
+  font-family: "Ubuntu", sans-serif;
 `;
