4674f5dc59f606596fa17a3c4a252e2409997c4c
Kirk <kirk.douglas.richardson@gmail.com>
2019-02-17 15:08:10 -0700

    Updates

---
+++ precache-manifest.822fe8aafefce3c3c19bca82598614b3.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "c3c86e71f309dde162a2e9b52cbc702f",
+    "url": "/simon-game/static/media/green.c3c86e71.mp3"
+  },
+  {
+    "revision": "bd3f4e66152b2ece5285",
+    "url": "/simon-game/static/css/main.8188e098.chunk.css"
+  },
+  {
+    "revision": "a42ef6d2557914365f29",
+    "url": "/simon-game/static/js/1.a42ef6d2.chunk.js"
+  },
+  {
+    "revision": "ce1747dc3c6073e37847",
+    "url": "/simon-game/static/js/runtime~main.ce1747dc.js"
+  },
+  {
+    "revision": "58045dabdc3a361cb9bb9faf2f1dd1f3",
+    "url": "/simon-game/static/media/font.58045dab.ttf"
+  },
+  {
+    "revision": "319c77102049da3622a954acd189f1eb",
+    "url": "/simon-game/static/media/red.319c7710.mp3"
+  },
+  {
+    "revision": "bd3f4e66152b2ece5285",
+    "url": "/simon-game/static/js/main.bd3f4e66.chunk.js"
+  },
+  {
+    "revision": "f4a912ee52e2c884a80499208c5568cc",
+    "url": "/simon-game/static/media/blue.f4a912ee.mp3"
+  },
+  {
+    "revision": "f3c18b98bab33c0fbedde9cb67bdfcf2",
+    "url": "/simon-game/static/media/yellow.f3c18b98.mp3"
+  },
+  {
+    "revision": "8849ef31e1867a74fad9c6f33accdb08",
+    "url": "/simon-game/static/media/success.8849ef31.wav"
+  },
+  {
+    "revision": "f768a2ef67d85005f5b43d33eade7959",
+    "url": "/simon-game/static/media/failure.f768a2ef.wav"
+  },
+  {
+    "revision": "de8c749b13195ca53810aeee2c89fa7c",
+    "url": "/simon-game/index.html"
+  }
+];
